f4bb459454bd9a176bf8629b8b55eb1d51769f12
LunaTW <68732727+LunaTW@users.noreply.github.com>
2021-04-24 16:02:18 +0800

    first commit

---
+++ src/main/java/com/docker/dockerdemoluna/DockerDemoLunaApplication.java
@@ -0,0 +1,13 @@
+package com.docker.dockerdemoluna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemoLunaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoLunaApplication.class, args);
+	}
+
+}
